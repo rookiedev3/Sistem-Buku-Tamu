86cef4e0fada7f7daf221a53222e76ccbeaa87a1
Anemoseelie <ridhoridho6879@gmail.com>
2026-08-03 12:08:40 +0700

    add migrates usersand guests

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,27 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->bigInteger('branch_id')->nullable();
+            $table->string('name', 150);
+            $table->string('email', 150)->unique();
+            $table->string('phone', 25);
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+            $table->enum('role', ['owner', 'manager', 'admin', 'pic', 'security']);
+            $table->boolean('is_active')->default(true);
+            $table->timestamp('last_login')->nullable();
         });
     }
 
