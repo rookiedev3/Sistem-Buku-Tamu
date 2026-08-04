57ef1984b9217c5c9f95243c6a7393be552ebe31
Zahwa Ayu Ramadhani <zahwaayu.stu@pnc.ac.id>
2026-08-04 13:05:29 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('branches', BranchesApiController::class);
+Route::apiResource('branches', BranchesApiController::class)->names([
+    'index'   => 'api.branches.index',
+    'store'   => 'api.branches.store',
+    'show'    => 'api.branches.show',
+    'update'  => 'api.branches.update',
+    'destroy' => 'api.branches.destroy',
+]);
